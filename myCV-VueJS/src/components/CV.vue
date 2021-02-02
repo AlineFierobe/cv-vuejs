f7b800a1b1032c73cce2948c2d005d66be0e5213
Aline Fierobe <contact@alinefierobe.com>
2021-02-02 13:17:57 +0100

    display data in CV

--- myCV-VueJS/src/components/CV.vue
+++ myCV-VueJS/src/components/CV.vue
@@ -1,124 +1,144 @@
 <template>
   <div>
-    <div>
-      <!-- HEADER WITH NAME AND SEARCHED POSITION -->
-      <header class="header">
-        <h1>PRENOM NOM</h1>
-        <h2>POSTE</h2>
-      </header>
-      <main class="main">
-        <!-- ASIDE -->
-        <aside>
-          <!-- CONTACT -->
-          <div class="contact">
-            <p class="contact-dispo">DISPO</p>
-            <p class="contact-mobilite">MOBILITE</p>
-            <h3 class="title">contact</h3>
-            <address class="contact-address">ADRESSE</address>
-            <p class="contact-email">EMAIL</p>
-            <p class="contact-phone">TELEPHONE</p>
-          </div>
+    <!-- HEADER WITH NAME AND SEARCHED POSITION -->
+    <header class="header" v-for="liste in liste" :key="liste.id">
+      <h1>{{ liste.prenom }} {{ liste.nom }}</h1>
+      <h2>
+        {{ liste.leTravail.poste }}
+      </h2>
+    </header>
+    <main class="main">
+      <!-- ASIDE -->
+      <aside>
+        <!-- CONTACT -->
+        <div class="contact" v-for="liste in liste" :key="liste.id">
+          <p class="contact-dispo">{{ liste.leTravail.disponibilite }}</p>
+          <p class="contact-mobilite">{{ liste.leTravail.mobilite }}</p>
+          <h3 class="title">contact</h3>
+          <address class="contact-address">{{ liste.adresse }}</address>
+          <p class="contact-email">{{ liste.email }}</p>
+          <p class="contact-phone">{{ liste.telephone }}</p>
+        </div>
 
-          <!-- SKILLS -->
+        <!-- SKILLS -->
+        <div v-for="liste in liste" :key="liste.id">
           <h3 class="title">skills</h3>
-          <ul id="listSkills">
+          <ul
+            id="listSkills"
+            v-for="competence in liste.lesCompetences"
+            :key="competence.id"
+          >
             <li class="modelSkills">
-              <h4>COMPETENCE</h4>
-              <div class="skills-progress">
-                <div
-                  class="skills-progress-bar"
-                  style="width:  NIVEAU  ;"
-                ></div>
-              </div>
+              <h4>{{ competence.competence }}</h4>
+              <p>{{ competence.niveau }}</p>
             </li>
           </ul>
-
-          <!-- LANGUAGES -->
+        </div>
+        <!-- LANGUAGES -->
+        <div v-for="liste in liste" :key="liste.id">
           <h3 class="title">languages</h3>
-          <ul id="listLanguages">
+          <ul
+            id="listLanguages"
+            v-for="langue in liste.lesLangues"
+            :key="langue.id"
+          >
             <li class="modelLanguages">
-              <h4>LANGUE</h4>
-              <p>NIVEAU</p>
+              <h4>{{ langue.langue }}</h4>
+              <p>{{ langue.niveau }}</p>
             </li>
           </ul>
+        </div>
 
-          <!-- ONLINE -->
-          <h3 class="title">online</h3>
-          <div class="listOnline">
-            <ul>
-              <li>
-                <a href="https:// WEBSITE"
-                  ><i class="fas fa-globe"></i> WEBSITE
-                </a>
-              </li>
-              <li>
-                <a href=" GITHUB"
-                  ><i class="fab fa-github"></i> github.com/ PRENOM NOM
-                </a>
-              </li>
-              <li>
-                <a href=" LINKEDIN"
-                  ><i class="fab fa-linkedin"></i> PRENOM NOM
-                </a>
+        <!-- ONLINE -->
+        <h3 class="title">online</h3>
+        <div class="listOnline" v-for="liste in liste" :key="liste.id">
+          <ul>
+            <li>
+              <a href="https://WEBSITE"
+                ><i class="fas fa-globe"></i> {{ liste.website }}
+              </a>
+            </li>
+            <li>
+              <a href="GITHUB"
+                ><i class="fab fa-github"></i> github.com/{{ liste.prenom
+                }}{{ liste.nom }}
+              </a>
+            </li>
+            <li>
+              <a href=" LINKEDIN"
+                ><i class="fab fa-linkedin"></i> {{ liste.prenom }}
+                {{ liste.nom }}
+              </a>
+            </li>
+          </ul>
+        </div>
+      </aside>
+
+      <!-- MAIN SECTION -->
+      <section>
+        <!-- ABOUT ME -->
+        <article class="aboutme" v-for="liste in liste" :key="liste.id">
+          <h3 class="title">about me</h3>
+          <p>{{ liste.presentation }}</p>
+        </article>
+
+        <!-- DEGREES -->
+        <article class="degrees">
+          <h3 class="title">degrees</h3>
+          <div v-for="liste in liste" :key="liste.id">
+            <ul
+              id="listDegrees"
+              v-for="degree in liste.lesDiplomes"
+              :key="degree.id"
+            >
+              <li class="modelDegrees">
+                <h4>{{ degree.diplome }}</h4>
+                <p class="modelDegrees-school">
+                  {{ degree.ecole }} , {{ degree.leLieu.ville }},
+                  <span> {{ degree.leLieu.pays }} </span>
+                </p>
+                <p class="modelDegrees-year">{{ degree.annee }}</p>
               </li>
             </ul>
           </div>
-        </aside>
+        </article>
 
-        <!-- MAIN SECTION -->
-        <section>
-          <!-- ABOUT ME -->
-          <article class="aboutme">
-            <h3 class="title">about me</h3>
-            <p>PRESENTATION</p>
-          </article>
-
-          <!-- DEGREES -->
-          <article class="degrees">
-            <h3 class="title">degrees</h3>
-            <div>
-              <ul id="listDegrees">
-                <li class="modelDegrees">
-                  <h4>DIPLOME</h4>
-                  <p class="modelDegrees-school">
-                    ECOLE , VILLE , <span> PAYS </span>
+        <!-- EXPERIENCES -->
+        <article class="experiences">
+          <h3 class="title">experiences</h3>
+          <div v-for="liste in liste" :key="liste.id">
+            <ul
+              id="listExperiences"
+              v-for="experience in liste.lesExperiences"
+              :key="experience.id"
+            >
+              <li class="modelExperiences">
+                <div class="modelExperiences-years">
+                  <span> {{ experience.debut }} </span>
+                  <span> {{ experience.fin }} </span>
+                </div>
+                <div class="modelExperiences-info">
+                  <h4>{{ experience.experience }}</h4>
+                  <p class="modelExperiences-info-company">
+                    {{ experience.entreprise }} ,
+                    {{ experience.leLieu.ville }} ,
+                    <span> {{ experience.leLieu.pays }} </span>
                   </p>
-                  <p class="modelDegrees-year">ANNEE</p>
-                </li>
-              </ul>
-            </div>
-          </article>
+                  <p class="modelExperiences-info-description">
+                    {{ experience.description }}
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </article>
+      </section>
+    </main>
 
-          <!-- EXPERIENCES -->
-          <article class="experiences">
-            <h3 class="title">experiences</h3>
-            <div>
-              <ul id="listExperiences">
-                <li class="modelExperiences">
-                  <div class="modelExperiences-years">
-                    <span> DEBUT </span> <span> FIN </span>
-                  </div>
-                  <div class="modelExperiences-info">
-                    <h4>POSTE</h4>
-                    <p class="modelExperiences-info-company">
-                      ENTREPRISE , VILLE , <span> PAYS </span>
-                    </p>
-                    <p class="modelExperiences-info-description">
-                      DESCRIPTION
-                    </p>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </article>
-        </section>
-      </main>
-
-      <!-- FOOTER -->
-      <footer class="footer">
-        <a href="WEBSITE">Aline Fierobe</a> &copy; 2021
-      </footer>
-    </div>
+    <!-- FOOTER -->
+    <footer class="footer">
+      <a href="WEBSITE">Aline Fierobe</a> &copy; 2021
+    </footer>
   </div>
 </template>
 
@@ -132,12 +152,17 @@ export default {
       liste: []
     };
   },
+  computed: {
+    list() {
+      return this.liste;
+    }
+  },
   created() {
     ajaxService
       .getList("listPersonne")
       .then(promise => {
         this.liste = promise;
-        console.log("Liste Personne ", this.liste);
+        console.log("Liste des Personnes ", this.liste);
       })
       .catch(error => console.log(error));
   }
